9e48d540a6d9de5423fe7b2a0aa679436563046c
Jeong Seongpil <16506343+jjhitel@users.noreply.github.com>
2025-12-10 22:20:29 +0900

    system: Delegate slot detection to DeviceController

--- bin/ltbox/actions/system.py
+++ bin/ltbox/actions/system.py
@@ -1,47 +1,17 @@
-import subprocess
-from typing import Optional, Dict
-
-from .. import constants as const
-from .. import utils, device
+from typing import Optional
+from .. import device
 from ..i18n import get_string
 from ..errors import ToolError
 
 def detect_active_slot_robust(dev: device.DeviceController) -> Optional[str]:
-    active_slot = None
-
-    if not dev.skip_adb:
-        try:
-            active_slot = dev.get_active_slot_suffix()
-        except Exception:
-            pass
-
-    if not active_slot:
-        if not dev.skip_adb:
-            try:
-                dev.reboot_to_bootloader()
-            except Exception as e:
-                raise ToolError(get_string("act_err_reboot_bl").format(e=e))
-        else:
-            print("\n" + "="*60)
-            print(get_string("act_manual_fastboot"))
-            print("="*60 + "\n")
-
-
-        dev.wait_for_fastboot()
-        active_slot = dev.get_active_slot_suffix_from_fastboot()
-
-        if not dev.skip_adb:
-            dev.fastboot_reboot_system()
-            dev.wait_for_adb()
-        else:
-            print("\n" + "="*60)
-            print(get_string("act_detect_complete"))
-            print(get_string("act_manual_edl"))
-            print("="*60 + "\n")
-
-    return active_slot
+    try:
+        return dev.detect_active_slot()
+    except Exception as e:
+        raise ToolError(get_string("act_warn_slot_fail")) from e
 
 def disable_ota(dev: device.DeviceController) -> None:
+    from .. import utils
+    
     utils.ui.echo(get_string("act_start_ota"))
     
     dev.wait_for_adb()
